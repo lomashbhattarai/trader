a275867157685e1f887fabf6b1d55c0a01e9f373
lomashbhattarai <lomashbhattarai1@gmail.com>
2020-03-14 11:05:49 +0545

    nav bar changes , navigation drawer added

--- src/App.vue
+++ src/App.vue
@@ -3,47 +3,70 @@
     <v-app-bar
       app
       color="blue"
-      
+      hide-on-scroll=true
     >
       <div class="d-flex align-center">
-        <router-link :to="{name:'home'}" tag="div">
-          <v-img
-            alt="Vuetify Logo"
-            class="shrink mr-2"
-            contain
-            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-            transition="scale-transition"
-            width="40"
-          />
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+        <router-link :to="{name:'home'}" class="mainTitle">
+           <h3>stockFly</h3>
           </router-link>
-          <h3> Stock Nepal </h3>
-        
       </div>
-
       <v-spacer></v-spacer>
-      <router-link :to="{name:'brokers'}">
-        <v-btn
-        text
-      >
-        <span class="mr-2">Brokers</span>
-      </v-btn>
-      </router-link>
-    
-      <router-link :to="{name:'watchlist'}">
-        <v-btn
-          text
-        >
-          <span class="mr-2">Watch list</span>
-        </v-btn>
-      </router-link>
             <v-btn
         text
       >
         <span class="mr-2">Login</span>
         <v-icon>mdi-account</v-icon>
-      </v-btn>
-      
+      </v-btn> 
     </v-app-bar>
+    <v-navigation-drawer
+        v-model="drawer"
+        :color="color"
+        :expand-on-hover="expandOnHover"
+        :mini-variant="miniVariant"
+        :right="false"
+        :src="bg"
+        absolute
+        dark
+        temporary
+      >
+        <v-list
+          dense
+          nav
+          class="py-0"
+        >
+          <v-list-item two-line :class="miniVariant && 'px-0'">
+            <v-list-item-avatar>
+              <img src="https://randomuser.me/api/portraits/men/81.jpg">
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>Name</v-list-item-title>
+              <v-list-item-subtitle>@username</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>
+                <router-link :to="{ name:item.routeName}">
+                {{ item.title }}
+                </router-link>
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
 
     <v-content>
       <keep-alive>
@@ -62,7 +85,26 @@ export default {
   },
 
   data: () => ({
-    //
+      drawer: false,
+      items:[{
+        title: 'Brokers',
+        routeName:'brokers',
+        icon:'  '
+      },
+      {
+        title: 'Watch list',
+        routeName:'watchlist',
+        icon:''
+      }
+      
+      ]
   }),
 };
 </script>
+
+<style  >
+.mainTitle {
+    text-decoration: none;
+    color: black !important; 
+}
+</style>
