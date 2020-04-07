b200d78bf7dd829971ae7012cf1d962b33df7303
lomashbhattarai <lomashbhattarai1@gmail.com>
2020-04-07 22:38:06 +0545

    changes

--- src/components/singlePage.vue
+++ src/components/singlePage.vue
@@ -1,33 +1,59 @@
 <template>
 <v-container class="mt-5">
     <router-link :to="{name:'home'}"><v-btn>Back</v-btn></router-link>
-    <v-select v-model="selectedCompany" 
-        hint="Pick a company to see its charts"
-        persistent-hint
-        :items="companiesSortedArray" @change="changeRoute()">
-         <template slot="selection" slot-scope="data">
-             {{ data.item.symbol }} - {{ data.item.name }}
-        </template>
-        <template slot="item" slot-scope="data">
-            {{ data.item.symbol }} - {{ data.item.name }}
-        </template>
-    </v-select>
-    <v-card class="mt-5">
-        <lineChart :symbol="symbol" 
-            :closingPriceArray="closingPriceArray"
-            :maxPriceArray="maxPriceArray"
-            :minPriceArray="minPriceArray" 
-            :days="dayArray"  
-             
-        />
-        <customBarChart class="mt-5"
-            :days="dayArray"
-            :volumeArray="volumeArray" />
-        <!-- supports and resistance -->
-    </v-card>
+    
+    <v-row>
+        <v-col cols="12" md="4">
+            <v-select v-model="selectedCompany" 
+                hint="Pick a company to see its charts"
+                persistent-hint
+                :items="companiesSortedArray" @change="changeRoute()">
+                <template slot="selection" slot-scope="data">
+                    {{ data.item.symbol }} - {{ data.item.name }}
+                </template>
+                <template slot="item" slot-scope="data">
+                    {{ data.item.symbol }} - {{ data.item.name }}
+                </template>
+            </v-select>
+    
+    <v-card
+    class="mx-auto mt-5"
+    max-width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="headline mb-1">{{ selectedCompany.symbol }}</v-list-item-title>
+        <v-list-item-subtitle> {{ selectedCompany.name }}</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn text color="blue">Add to Portfolio</v-btn>
+      <v-btn text color="pink" @click="addToWatchlist(company)">
+        Add to Watchlist
+        <!-- <v-icon title="Add to watch list">mdi-heart-outline</v-icon> -->
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+    </v-col>
+        <v-col cols="12" md="8">
+            <v-card class="mt-5">
+                <lineChart :symbol="symbol" 
+                    :closingPriceArray="closingPriceArray"
+                    :maxPriceArray="maxPriceArray"
+                    :minPriceArray="minPriceArray" 
+                    :days="dayArray"  
+                    
+                />
+                <customBarChart class="mt-5"
+                    :days="dayArray"
+                    :volumeArray="volumeArray" />
+                <!-- supports and resistance -->
+            </v-card>
+        </v-col>
+    </v-row>
 </v-container>   
-    
-    
 </template>
 
 <script>
